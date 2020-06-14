7768f3e5dc8febc22ee3875deb2999b9fbc93125
calebe <calebesouzavasoncelos@gmail.com>
2020-06-14 19:15:10 -0300

    Estruturando card

--- lib/main.dart
+++ lib/main.dart
@@ -46,13 +46,54 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-         Column(
-           children: _transactions.map((tr) {
-             return Card (
-               child: Text(tr.title)
-             );
-           }).toList(),
-         )
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.amber,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      tr.value.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.amber,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        tr.date.toString(),
+                         style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ));
+            }).toList(),
+          )
         ],
       ),
     );
